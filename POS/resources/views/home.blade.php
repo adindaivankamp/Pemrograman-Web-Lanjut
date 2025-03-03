340c717713f26b18eca171cd1ee9039fe0fa4fb2
Adinda Ivanka <adindaivankamaysandaputri@gmail.com>
2025-03-03 08:39:13 +0700

    merapikan

---
+++ POS/resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>Home - POS</title>
+</head>
+<body>
+    <h1>Selamat Datang di Aplikasi POS</h1>
+    <nav>
+        <ul>
+            <li><a href="/category/food-beverage">Food & Beverage</a></li>
+            <li><a href="/category/beauty-health">Beauty & Health</a></li>
+            <li><a href="/category/home-care">Home Care</a></li>
+            <li><a href="/category/baby-kid">Baby & Kid</a></li>
+            <li><a href="/sales">Penjualan</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
---
+++ POS/resources/views/products/food-beverage.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>Food & Beverage</title>
+</head>
+<body>
+    <h1>Daftar Produk: Food & Beverage</h1>
+    <ul>
+        <li>Teh Botol Sosro</li>
+        <li>Indomie Goreng</li>
+        <li>Kopi Kapal Api</li>
+    </ul>
+    <a href="/">Kembali ke Home</a>
+</body>
+</html>
