90d6aebb98f5ea85f9c168609000b62f46c59de2
Adinda Ivanka <adindaivankamaysandaputri@gmail.com>
2025-03-10 09:36:08 +0700

    seed

--- Minggu 4/database/seeders/PenjualanSeeder.php
+++ Minggu 4/database/seeders/PenjualanSeeder.php
@@ -23,14 +23,14 @@ class PenjualanSeeder extends Seeder
         $data = [
             ['user_id' => 3, 'pembeli' => 'Adinda', 'penjual_kode' => 'P001', 'penjualan_tanggal' => Carbon::now()],
             ['user_id' => 3, 'pembeli' => 'Farel', 'penjual_kode' => 'P002', 'penjualan_tanggal' => Carbon::now()->subDay()],
-            ['user_id' => 3, 'pembeli' => 'Shaquil', 'penjual_kode' => 'P003', 'penjualan_tanggal' => Carbon::now()->subDays(2)],
-            ['user_id' => 3, 'pembeli' => 'Dina', 'penjual_kode' => 'P004', 'penjualan_tanggal' => Carbon::now()->subDays(3)],
-            ['user_id' => 3, 'pembeli' => 'Aida', 'penjual_kode' => 'P005', 'penjualan_tanggal' => Carbon::now()->subDays(4)],
-            ['user_id' => 3, 'pembeli' => 'Dela', 'penjual_kode' => 'P006', 'penjualan_tanggal' => Carbon::now()->subDays(5)],
-            ['user_id' => 3, 'pembeli' => 'Rani', 'penjual_kode' => 'P007', 'penjualan_tanggal' => Carbon::now()->subDays(6)],
-            ['user_id' => 3, 'pembeli' => 'Yuta', 'penjual_kode' => 'P008', 'penjualan_tanggal' => Carbon::now()->subDays(7)],
-            ['user_id' => 3, 'pembeli' => 'Niriza', 'penjual_kode' => 'P009', 'penjualan_tanggal' => Carbon::now()->subDays(8)],
-            ['user_id' => 3, 'pembeli' => 'Danen', 'penjual_kode' => 'P010', 'penjualan_tanggal' => Carbon::now()->subDays(9)],
+            ['user_id' => 3, 'pembeli' => 'Dini', 'penjual_kode' => 'P003', 'penjualan_tanggal' => Carbon::now()->subDays(2)],
+            ['user_id' => 3, 'pembeli' => 'Billy', 'penjual_kode' => 'P004', 'penjualan_tanggal' => Carbon::now()->subDays(3)],
+            ['user_id' => 3, 'pembeli' => 'Udin', 'penjual_kode' => 'P005', 'penjualan_tanggal' => Carbon::now()->subDays(4)],
+            ['user_id' => 3, 'pembeli' => 'Yudi', 'penjual_kode' => 'P006', 'penjualan_tanggal' => Carbon::now()->subDays(5)],
+            ['user_id' => 3, 'pembeli' => 'Kyara', 'penjual_kode' => 'P007', 'penjualan_tanggal' => Carbon::now()->subDays(6)],
+            ['user_id' => 3, 'pembeli' => 'Karin', 'penjual_kode' => 'P008', 'penjualan_tanggal' => Carbon::now()->subDays(7)],
+            ['user_id' => 3, 'pembeli' => 'Layla', 'penjual_kode' => 'P009', 'penjualan_tanggal' => Carbon::now()->subDays(8)],
+            ['user_id' => 3, 'pembeli' => 'Ruby', 'penjual_kode' => 'P010', 'penjualan_tanggal' => Carbon::now()->subDays(9)],
         ];
 
         DB::table('t_penjualan')->insert($data);
