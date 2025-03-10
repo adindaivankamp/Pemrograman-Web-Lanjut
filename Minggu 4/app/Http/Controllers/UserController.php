98e2e99a14de54e9638429a580179918013ebe11
Adinda Ivanka <adindaivankamaysandaputri@gmail.com>
2025-03-10 11:53:48 +0700

    Praktikum 2.1

--- Minggu 4/app/Http/Controllers/UserController.php
+++ Minggu 4/app/Http/Controllers/UserController.php
@@ -8,33 +8,12 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    // public function index()
-    // {
-    //     $data = [
-    //         'level_id' => 2,
-    //         'username' => 'manager_dua',
-    //         'nama' => 'Manager 2',
-    //         'password' => Hash::make('12345')
-    //     ];
-        
-    //     UserModel::create($data);
-
-    //     //coba akses model usermodel
-    //     $user = UserModel::all(); //ambil semua data dari tabel m_user
-    //     return view('user', ['data' => $user]);
-    // }
-
+   
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-    
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
