e974913947066db6b5294f433dcbd94ca566475f
Adinda Ivanka <adindaivankamaysandaputri@gmail.com>
2025-04-10 20:55:38 +0700

    Praktikum 2

--- Minggu 6/routes/web.php
+++ Minggu 6/routes/web.php
@@ -44,6 +44,8 @@ Route::group(['prefix' => 'user'], function () {
     Route::post('/', [UserController::class, 'store']);        // menyimpan data user baru
     Route::get('/create_ajax', [UserController::class, 'create_ajax']);  // Menampilkan halaman form tambah user Ajax
     Route::post('/ajax', [UserController::class, 'store_ajax']);        // Menyimpan data user baru ajax
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);        // Menampilkan halaman form edit user Ajax
+    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);    // Menyimpan perubahan data user Ajax
     Route::get('/{id}', [UserController::class, 'show']);        // menampilkan detail user
     Route::get('/{id}/edit', [UserController::class, 'edit']);    // menampilkan halaman form edit user
     Route::put('/{id}', [UserController::class, 'update']);      // menyimpan perubahan data user
