a4549c8944d59057fa98173ac8ddbabb8479adb6
Adinda Ivanka <adindaivankamaysandaputri@gmail.com>
2025-04-12 11:24:18 +0700

    Praktikum 1

--- Minggu 7/app/Models/UserModel.php
+++ Minggu 7/app/Models/UserModel.php
@@ -2,24 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class UserModel extends Model
 {
+
     use HasFactory;
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
+    protected $hidden = ['password']; // jangan di tampilkan saat select
+    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
+
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
+     * Relasi ke tabel Level
      */
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
-    
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
