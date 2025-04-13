4d649149f9929eac2059498831c1fe42bb0a55f4
Adinda Ivanka <adindaivankamaysandaputri@gmail.com>
2025-04-13 16:25:12 +0700

    regristrasi akun

--- POS/app/Models/UserModel.php
+++ POS/app/Models/UserModel.php
@@ -2,14 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
-
     use HasFactory;
 
     protected $table = 'm_user';
@@ -19,10 +18,36 @@ class UserModel extends Model
     protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
 
     /**
-     * Relasi ke tabel Level
+     * Relasi ke tabel level
      */
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    //authorizatoon
+    /**
+     * Mendapatkan nama role
+     */
+    public function getRoleName(): string
+    {
+        return $this->level->level_nama;
+    }
+
+    /**
+     * Cek apakah user memiliki role tertentu
+     */
+    public function hasRole($role): bool
+    {
+        return $this->level->level_kode === $role; 
+    }
+
+    //multilevel authorization
+    /**
+     * Mendapatkan kode role
+     */
+        public function getRole()
+    {
+        return $this->level->level_kode;
     }
 }
