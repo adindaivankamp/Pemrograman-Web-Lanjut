d58075960ac0183e8cd04a4abd1b304c61ba932b
Adinda Ivanka <adindaivankamaysandaputri@gmail.com>
2025-04-13 12:59:46 +0700

    Tugas 3

--- Minggu 7/app/Http/Middleware/AuthorizeUser.php
+++ Minggu 7/app/Http/Middleware/AuthorizeUser.php
@@ -8,23 +8,18 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AuthorizeUser
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
     public function handle(Request $request, Closure $next, ...$roles): Response
     {
-        $user = $request->user();
-
-        // Cek dulu apakah user-nya null (belum login)
-        if (!$user) {
-            return redirect()->route('login'); // Atau arahkan ke rute login kamu
+        $user_role = $request->user()->getRole();  // ambil data level_kode dari user yg login
+        if (in_array($user_role, $roles)) {  // cek apakah level_kode user ada di dalam array roles
+            return $next($request); // jika ada, maka lanjutkan request
         }
-
-        $user_role = $user->getRole(); // Baru ambil role-nya
-
-        // Cek apakah role-nya termasuk yang diizinkan
-        if (in_array($user_role, $roles)) {
-            return $next($request);
-        }
-
-        // Kalau role-nya gak sesuai, kasih forbidden
-        abort(403, 'Unauthorized');
+        // jika tidak punya role, maka tampilkan error 403
+        abort(403, 'Forbidden. Kamu tidak punya akses ke halaman ini');
     }
 }
