c9c3b339c670a2df28a0f21eac7e38b3e3f27146
Adinda Ivanka <adindaivankamaysandaputri@gmail.com>
2025-04-15 16:20:26 +0700

    Laporan Jobsheet 8 dan perubahan POS

--- POS/resources/views/welcome.blade.php
+++ POS/resources/views/welcome.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.template')
- 
- @section('content')
- <div class="card">
-     <div class="card-header">
-         <h3 class="card-title">Halo, apakabar!!!</h3>
-         <div class="card-tools"></div>
-     </div>
-     <div class="card-body">
-         Selamat datang semua, ini adalah halaman utama dari aplikasi ini.
-     </div>
- </div>
- @endsection
+
+@section('content')
+<div class="p-6 bg-white rounded-xl shadow-md text-center">
+    <h2 class="text-2xl font-semibold text-blue-700 mb-4">👋 Selamat Datang!</h2>
+    <p class="text-gray-700 text-lg leading-relaxed">
+        Senang banget bisa menyambut Anda di sini. <br>
+        Semoga kehadiran Anda jadi awal dari pengalaman yang menyenangkan dan bermanfaat bersama kami. <br><br>
+        Silakan eksplorasi fitur-fitur yang ada. Kalau butuh bantuan, kami selalu siap membantu. <br>
+        Semoga harimu menyenangkan! ✨
+    </p>
+</div>
+@endsection
