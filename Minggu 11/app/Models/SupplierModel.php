5a63ede8dc7fda001e401218fba0515dc4cf46d1
Adinda Ivanka <adindaivankamaysandaputri@gmail.com>
2025-04-27 21:43:27 +0700

    Jobsheet 11

---
+++ Minggu 11/app/Models/SupplierModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SupplierModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_supplier'; // nama tabel di database
+    protected $primaryKey = 'supplier_id'; // ganti dengan primary key yang sesuai
+    protected $fillable = ['supplier_kode','supplier_nama', 'supplier_alamat'];
+}
---
+++ Minggu 11/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.template')
+
+@section('content')
+<div class="p-6 bg-white rounded-xl shadow-md text-center">
+    <h2 class="text-2xl font-semibold text-blue-700 mb-4">👋 Selamat Datang!</h2>
+    <p class="text-gray-700 text-lg leading-relaxed">
+        Senang banget bisa menyambut Anda di sini. <br>
+        Semoga kehadiran Anda jadi awal dari pengalaman yang menyenangkan dan bermanfaat bersama kami. <br><br>
+        Silakan eksplorasi fitur-fitur yang ada. Kalau butuh bantuan, kami selalu siap membantu. <br>
+        Semoga harimu menyenangkan! ✨
+    </p>
+</div>
+@endsection
