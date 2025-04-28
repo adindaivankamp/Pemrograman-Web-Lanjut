ac4ee5861168b69a049cff6866afee5965de2762
Adinda Ivanka <adindaivankamaysandaputri@gmail.com>
2025-04-28 12:54:36 +0700

    Tugas

--- Minggu 11/app/Models/BarangModel.php
+++ Minggu 11/app/Models/BarangModel.php
@@ -1,22 +1,41 @@
 <?php
- 
- namespace App\Models;
- 
- use Illuminate\Database\Eloquent\Factories\HasFactory;
- use Illuminate\Database\Eloquent\Model;
- use App\Models\KategoriModel;
- 
- class BarangModel extends Model
- {
-     use HasFactory;
- 
-     protected $table = 'm_barang';
-     protected $primaryKey = 'barang_id';
-     protected $fillable = ['kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual'];
- 
-     public function kategori()
-     {
-         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
-     }
- 
- }
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class BarangModel extends Model
+{
+    use HasFactory;
+    protected $table = 'm_barang';
+    protected $primaryKey = 'barang_id';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'barang_kode',
+        'barang_nama',
+        'kategori_id',
+        'harga_beli',
+        'harga_jual',
+        'image'
+    ];
+
+    public function kategori(): BelongsTo 
+    {
+        return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => $image ? url('/storage/barang/' . $image) : null
+        );
+    }
+
+}
